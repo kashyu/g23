ad88e1d5587bab4d2089bf4d16261e5bc9158285
kashyu <20723500+kashyu@users.noreply.github.com>
2022-11-17 13:33:54 +0000

    Update and rename wirelesss.py to thingspeak.py

--- thingspeak.py
+++ thingspeak.py
@@ -1,6 +1,6 @@
 import time
 import serial
-from urlib.request import urlopen
+from urllib.request import urlopen
 import re
 import urllib
     ser = serial.Serial(
@@ -13,7 +13,7 @@ import urllib
 )
 counter=0
 ser.flushInput()
-url1='https://api.thingspeak.com/update?api_key=6KCAKZJ5RMHW9A2Q=0'
+url1='https://api.thingspeak.com/update?api_key=6KCAKZJ5RMHW9A2Q&field=0'
 while 1:
     data_string=ser.readline().decode()
     data_float= re.findall('\d+(?:\.\d+)?',data_string)
@@ -21,16 +21,15 @@ while 1:
     if NID=="1":
         Light=data_float[2]
         Temperature=data_float[3]
-        motion=data_float[4]
-    elif NID=="2":
-        Light2=data_float[2]
-        Temperature2=data_float[2]
+        Motion=data_float[4]
     elif NID=="3":
-        Water=data_float[2]
+        Moisture=data_float[2]
+        Temperature2=data_float[3]
+        Light2=data_float[4]
     counter=counter+1
     print (data_float)
     if counter>=3:
-        upload1=urllib2.urlopen (url1+ Light +"&field2=0"+Temperature+"&field3=0"+motion +"&field4=0"+     +"&field5=0"+  "&field6=0"+  "&field7=0"+  "&field8=0"+ )
+        upload1=urllib2.urlopen (url1+Light+"&field2=0"+Temperature+"&field3=0"+Motion+"&field6=0"+Moisture+"&field7=0"+Temperature2+"&field8=0"+Light2)
         upload1.read()
         upload1.close()
         counter=0
